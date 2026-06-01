aab203257fc02389f771157d13c1fd385e60b0b1
Maxdeoro <maxdeoro@gmail.com>
2026-06-01 23:17:06 +0300

    divide App into separate components

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import '../index.css';
+import downloadButtonFile from '../utilites/dounloadButtonFile';
+
+function Header({showInstructions, toggleInstructions}){
+  return (
+    <header className="app-header">
+        <h1>UI Component Library</h1>
+        <p>Explore reusable and customizable components</p>
+        <div className="header-buttons">
+          <button className="accordion-button" onClick={toggleInstructions}>
+            {showInstructions ? "Hide Instructions" : "Show Instructions"}
+          </button>
+          <button onClick={downloadButtonFile} className="download-button">
+            Download Button.js
+          </button>
+        </div>
+      </header>
+  )
+};
+
+export default Header;
